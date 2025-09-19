450a2f4f51bbcd18d78ba8f6537f64dd21811da6
Timbo <tim@refsix.com>
2025-09-19 08:59:21 +0100

    fix

--- game/vite.config.ts
+++ game/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/mind-frag-monorepo/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/mind-fragment/' : '/',
   server: {
     port: 3001
   }

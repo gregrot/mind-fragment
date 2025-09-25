ebf97a5abb92fd75fac47f7b8ebd86ef652fa7a5
Timbo <tim@refsix.com>
2025-09-25 07:52:32 +0100

    removed storybook for now

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,15 +2,14 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: ['@storybook/addon-docs'],
   staticDirs: ['../public'],
+
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
   webpackFinal: async (config) => {
     config.module?.rules?.push({
       test: /\.tsx?$/,
@@ -43,7 +42,7 @@ const config: StorybookConfig = {
     }
 
     return config;
-  },
+  }
 };
 
 export default config;
